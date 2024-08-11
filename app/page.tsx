ea83fdc0fdd2350b2a17f94422f9cdf34126120c
Bhuneshvar <imankitkalirawana@gmail.com>
2024-08-11 08:26:06 +0530

    added work

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import BackgroundGradient from "@/components/components/BackgroundGradient";
 import Banner from "@/components/sections/Banner";
 import Navbar from "@/components/sections/Navbar";
+import Projects from "@/components/sections/Projects";
 import Showreel from "@/components/sections/Showreel";
 import Work from "@/components/sections/Work";
 import { Divider } from "@nextui-org/react";
@@ -8,15 +9,16 @@ import { Divider } from "@nextui-org/react";
 export default function Home() {
   return (
     <>
-      <div className=" overflow-hidden select-none">
+      <div className="select-none">
         <BackgroundGradient />
         <div className="max-w-7xl mx-auto p-8">
           <Navbar />
           <Banner />
           <Divider className="mb-12" />
           <Showreel />
-          <Work />
         </div>
+        <Work />
+        <Projects />
       </div>
     </>
   );
--- components/sections/Work.tsx
+++ components/sections/Work.tsx
@@ -1,11 +1,123 @@
-import React from "react";
+"use client";
+import { MeteoconsStar, PhStarFourFill } from "@/components/components/icons";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
 
 const Work = () => {
   return (
-    <>
-      <div className="mt-24"></div>
-    </>
+    <div>
+      <HorizontalScrollCarousel />
+    </div>
+  );
+};
+
+const HorizontalScrollCarousel = () => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    // offset: ["start end", "end start"],
+  });
+
+  // Transform to move from right (100%) to left (-100%)
+  const x = useTransform(scrollYProgress, [0, 1], ["52%", "-150%"]);
+
+  return (
+    <section ref={targetRef} className="relative h-[300vh]">
+      <div className="sticky top-0 flex h-screen items-end pb-8 overflow-hidden">
+        <div className="text-[80px] absolute top-0 left-[50%] translate-x-[-50%] leading-[80px] text-center mt-20">
+          <MeteoconsStar
+            className="text-[#ec6ed5] absolute -left-24 top-[40%]"
+            fontSize={70}
+          />
+          <PhStarFourFill
+            className="text-[#ec6ed5] absolute left-[110%] top-[0%]"
+            fontSize={50}
+          />
+          <div>What I</div>
+          <div className="flex gap-2">
+            <span className="font-ppneuemigraitalicbold text-[#30bdd6] -rotate-12 block">
+              Realllly
+            </span>
+            <span>Do?</span>
+          </div>
+        </div>
+        <img src="/shape-2.svg" className="absolute -bottom-36" alt="" />
+
+        <motion.div style={{ x }} className="flex gap-8 w-[700vw]">
+          {cards.map((card) => (
+            <Card card={card} key={card.id} />
+          ))}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+const Card = ({ card }: { card: CardType }) => {
+  return (
+    <div
+      key={card.id}
+      className="group rounded-[30px] p-8 bg-[#2a272ea9] backdrop-blur-sm relative h-[400px] w-[450px] overflow-hidden"
+    >
+      <img src={card.url} width={150} />
+      <h3 className="text-[32px] mt-4">{card.title}</h3>
+      <p className=" font-ppneuemachinaregular mt-4 text-[16px]">
+        {card.description}
+      </p>
+    </div>
   );
 };
 
 export default Work;
+
+type CardType = {
+  url: string;
+  title: string;
+  id: number;
+  icon: string;
+  description: string;
+};
+
+const cards: CardType[] = [
+  {
+    url: "/1.svg",
+    title: "Title 1",
+    id: 1,
+    icon: "lucide:shapes",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    url: "/2.svg",
+    title: "Title 2",
+    id: 2,
+    icon: "lucide:shapes",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    url: "/3.svg",
+    title: "Title 3",
+    id: 3,
+    icon: "lucide:shapes",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    url: "/4.svg",
+    title: "Title 4",
+    id: 4,
+    icon: "lucide:shapes",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    url: "/5.svg",
+    title: "Title 5",
+    id: 5,
+    icon: "lucide:shapes",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+];
