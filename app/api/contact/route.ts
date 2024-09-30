7a69df9847d93440efb3ccd37ff8c2d5ed264611
Your Name <imankitkalirawana@gmail.com>
2024-09-30 18:03:58 +0530

    refactor: Update contact route and nodemailer configuration
    Simplify and improve the contact route by updating the GET and POST methods. Also, refactor the nodemailer configuration to use a different email service provider.

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,29 +2,29 @@ import { transporter } from "@/utils/nodemailer";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    try {
-        return NextResponse.json({ message: "Hello World" });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.error();
-    }
+  try {
+    return NextResponse.json({ message: "Hello World" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
 }
 
 export async function POST(request: Request) {
-    try {
-        const data = await request.json();
-        await transporter.sendMail({
-            from: {
-                name: "Divinely Developer",
-                address: process.env.NEXT_PUBLIC_GMAIL || "",
-            },
-            to: "divinelydeveloper@gmail.com",
-            subject: "New message from Divinely Developer Portfolio",
-            text: `Name: ${data.name}\nEmail: ${data.email}\nPhone: ${data.phone}\nMessage: ${data.message}`,
-        });
-        return NextResponse.json({ message: "Message sent successfully" });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.error();
-    }
+  try {
+    const data = await request.json();
+    await transporter.sendMail({
+      from: {
+        name: `${data.name} from Divinely Developer Portfolio`,
+        address: data.email || "unknown@divinely.dev",
+      },
+      to: process.env.EMAIL || "contact@divinely.dev",
+      subject: `New message from ${data.name}`,
+      text: `Name: ${data.name}\nEmail: ${data.email}\nPhone: ${data.phone}\nMessage: ${data.message}`,
+    });
+    return NextResponse.json({ message: "Message sent successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
 }
--- utils/nodemailer.ts
+++ utils/nodemailer.ts
@@ -1,14 +1,14 @@
-import nodemailer from "nodemailer";
-const email = process.env.NEXT_PUBLIC_GMAIL;
-const password = process.env.NEXT_PUBLIC_GMAIL_PASSWORD;
+// import nodemailer from "nodemailer";
+// const email = process.env.NEXT_PUBLIC_GMAIL;
+// const password = process.env.NEXT_PUBLIC_GMAIL_PASSWORD;
 
-export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: email,
-        pass: password,
-    },
-});
+// export const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: email,
+//         pass: password,
+//     },
+// });
 
 // export const mailOptions = {
 //     from: {
@@ -20,3 +20,18 @@ export const transporter = nodemailer.createTransport({
 //     subject: "",
 //     text: "",
 // };
+
+import nodemailer from "nodemailer";
+
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+
+export const transporter = nodemailer.createTransport({
+  host: "smtpout.secureserver.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: email,
+    pass: password,
+  },
+});
